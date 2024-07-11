b550b1e78f39f84fb6dbf18d4c7216e218b55023
Jawad Ahmed Tareen <jtareen440@gmail.com>
2024-07-12 01:16:08 +0500

    implemented polynomial regression using scikit learn

--- Course 1/scikitLearn/linearRegression.py
+++ Course 1/scikitLearn/linearRegression.py
@@ -1,32 +1,45 @@
-import matplotlib.pyplot as plt
-import numpy as np
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+import os
 
-data = pd.read_csv('E:/Courses/AI deeplearning.AI/Course 1/module 1/linear regression/advertising.csv')
+# get csv path
+script_dir = os.path.dirname(os.path.abspath(__file__))
+csv_path = os.path.join(script_dir, 'advertising.csv')
+
+# get data from csv through pandas
+data = pd.read_csv(csv_path)
+
+data.head()
 
 # data set
 x = np.array(data['TV']).reshape(-1,1)
-y = np.array(data['Sales']).reshape(-1,1)
+y = np.array(data['Sales'])
 
+def visualize_model(x_train, y_train, x_pred, y_pred):
+    plt.title('Linear regression visualization')
+
+    plt.plot(x_pred, y_pred, color = 'red', label = 'prediction model')
+    plt.scatter(x_train, y_train, color = 'blue', label = 'data points')
+
+    plt.xlabel('TV')
+    plt.ylabel('Price')
+
+    plt.legend()
+    plt.show()
+
+# make model
 model = LinearRegression()
 
+# feed data to the model
 model.fit(x,y)
 
-d = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+# predict values of y by model
+y_pred = model.predict(x)
 
-print('w : ',model.coef_)
-print('b : ',model.intercept_)
+# check the score of predicted values of y againts actual values of y
+print(r2_score(y, y_pred))
 
-y_pred = model.coef_ * x + model.intercept_
-
-plt.title('Linear regression visualization')
-
-plt.plot(x,y_pred, color = 'red', label = 'prediction model')
-plt.scatter(x,y, color = 'blue', label = 'data points')
-
-plt.xlabel('TV')
-plt.ylabel('Price')
-
-plt.legend()
-plt.show()
+visualize_model(x, y, x, y_pred)
---
+++ Course 1/scikitLearn/polynomialRegression.py
@@ -0,0 +1,51 @@
+from sklearn.metrics import r2_score
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+csv_path = os.path.join(script_dir, 'advertising.csv')
+
+data = pd.read_csv(csv_path)
+
+# data set
+x_train = np.array(data['TV']).reshape(-1, 1)
+y_train = np.array(data['Sales'])
+
+def train_polynomial_model(x_train, y_train, deg):
+    # make a polynomial with degree 3
+    model_polynomial_features = PolynomialFeatures(degree=deg)
+
+    # make a polynomial model with a pipeline
+    model = make_pipeline(model_polynomial_features, LinearRegression())
+
+    # fit data to the model
+    model.fit(x_train, y_train)
+
+    return model
+
+def visualize_model(x_train, y_train, x_pred, y_pred):
+    plt.title('Linear regression visualization')
+
+    plt.plot(x_pred, y_pred, color = 'red', label = 'prediction model')
+    plt.scatter(x_train, y_train, color = 'blue', label = 'data points')
+
+    plt.xlabel('TV')
+    plt.ylabel('Price')
+
+    plt.legend()
+    plt.show()
+
+# train model
+model = train_polynomial_model(x_train, y_train, 5)
+
+x_pred = np.linspace(x_train.min(), x_train.max(), 100)[:, np.newaxis]
+y_pred = model.predict(x_pred)
+
+print(r2_score(y_train, model.predict(x_train)))
+
+visualize_model(x_train, y_train, x_pred, y_pred)
