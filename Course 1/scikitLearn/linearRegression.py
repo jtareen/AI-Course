d11729fc2f6dbeee0ec6ed0afe482dc213c6673d
Jawad Ahmed Tareen <jtareen440@gmail.com>
2024-07-11 22:36:52 +0500

    applied linear regression using scikit learn

--- Course 1/scikitLearn/linearRegression.py
+++ Course 1/scikitLearn/linearRegression.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.linear_model import LinearRegression
-import math, os
 import pandas as pd
 
 data = pd.read_csv('E:/Courses/AI deeplearning.AI/Course 1/module 1/linear regression/advertising.csv')
@@ -14,10 +13,20 @@ model = LinearRegression()
 
 model.fit(x,y)
 
-print('w : ',model.intercept_)
-print('b : ',model.coef_)
+d = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
 
+print('w : ',model.coef_)
+print('b : ',model.intercept_)
 
-plt.scatter(x,y)
+y_pred = model.coef_ * x + model.intercept_
 
+plt.title('Linear regression visualization')
+
+plt.plot(x,y_pred, color = 'red', label = 'prediction model')
+plt.scatter(x,y, color = 'blue', label = 'data points')
+
+plt.xlabel('TV')
+plt.ylabel('Price')
+
+plt.legend()
 plt.show()
